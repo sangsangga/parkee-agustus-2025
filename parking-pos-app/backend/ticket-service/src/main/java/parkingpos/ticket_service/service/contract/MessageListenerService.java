6e57da30d3b752b7eff47027b641f03a5e5f43cb
sangga <wandaputrasangga@gmail.com>
2025-08-07 12:35:07 +0700

    remove rabbitmq

--- parking-pos-app/backend/ticket-service/src/main/java/parkingpos/ticket_service/service/contract/MessageListenerService.java
+++
@@ -1,7 +0,0 @@
-package parkingpos.ticket_service.service.contract;
-
-import parkingpos.ticket_service.dto.Messaging.CheckinMessageDTO;
-
-public interface MessageListenerService {
-    void handleCheckinRequest(CheckinMessageDTO message);
-}
